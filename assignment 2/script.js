255509642fd9f9f9de3d195deca2e4f1e5e8e512
shivamishra6880 <147376259+shivamishra6880@users.noreply.github.com>
2024-05-25 13:05:53 +0530

    Add files via upload

---
+++ assignment 2/script.js
@@ -0,0 +1,48 @@
+// 1.
+let a= 10;
+let b = 50;
+console.log(a+b);
+
+// 2.
+let d = 5;
+let c = 3;
+console.log(d>c)
+
+// 3.
+let e =15;
+let f = 20;
+if(e%10 == 0 || f%10 == 0){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+
+// 4.
+
+let g =4586;
+g= 4586/1000;
+console.log(Math.floor(g))
+
+// 5.
+
+let h = 8648;
+h= 8648%10;
+console.log(h)
+
+// 7.
+
+let i = 545;
+let j = 486;
+k = i*j;
+console.log(k)
+
+// 8.
+
+let suba= 50;
+let subb =20;
+let subc= 100;
+sum = suba +subb+ subc;
+av = sum/3;
+console.log('total mark ='+sum)
+console.log("avrage of marks ="+av)
